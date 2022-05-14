414d94d2026e16d35e81017610fb1aec9382b08b
Vagisha Misra <70707224+Vagisha-Misra@users.noreply.github.com>
2022-05-14 23:22:28 +0530

    Day 7

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -6,6 +6,10 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// Approach 1
+//     uses set or hash table or map
+//     TC = O(n)
+//     SC = O(n)
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
@@ -18,6 +22,31 @@ public:
             }
             s.insert(curr);
             curr = curr->next;
+        }
+        return false;
+    }
+};
+
+
+// Approach 2
+//     2 Pointer Approach
+//     if the ll has cycle then fast and slow pointers will coincide 
+//     return true if this happens
+//     else if fast reaches the NULL ptr or fast->next is null 
+//     then stop traversing and return false
+//        TC = O(n)
+//        SC = O(1)
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        if(head == NULL)return false;
+        while(fast!=NULL && fast->next!=NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow)return true;
         }
         return false;
     }
